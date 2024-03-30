e4319b6edf01f5aa7d48b6c62454c1dbadf54216
esther57-web <kamardine.ossete@gmail.com>
2024-03-30 11:54:32 +0100

    incomplete filter function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const galleryElement = document.querySelector(".gallery");
 const filterElement = document.querySelector(".filter");
 
+
 const allFilterBtn = [];
 
 async function initGallery() {
@@ -12,6 +13,7 @@ async function initGallery() {
     for (const objet of data) {
       const figure = document.createElement("figure");
       galleryElement.appendChild(figure);
+      figure.classList.add("figure")
 
       const image = new Image();
       image.src = objet.imageUrl;
@@ -27,25 +29,52 @@ async function initGallery() {
     const uniqueCategories = [...new Set(categories)];
     uniqueCategories.unshift("Tout");
 
-    //création de la section filtre
-
     for (const category of uniqueCategories) {
       const filterBtn = document.createElement("button");
       filterBtn.textContent = category;
       filterBtn.classList.add("filter-btn");
-      // tout par défault
-      filterBtn.addEventListener("click", () => {
-        const allFilterBtn = document.querySelectorAll(".filter-btn");
-        for (const currentFilterBtn of allFilterBtn) {
-          if (currentFilterBtn === filterBtn) {
-            currentFilterBtn.classList.add("active-filter-btn");
-            //afficher
-          } else {
-            currentFilterBtn.classList.remove("active-filter-btn");
-            //masquer
-          }
+      
+      function filtrer(filtre) {
+        filtre = "Tout";
+
+        // Je veux ajouter une classe active au filterBtn qui a un textContext égal à "Tout" mais ça l'ajoute à tous les boutons
+        if (filtre === "Tout") {
+            filterBtn.classList.add("active-filter-btn");
         }
-      });
+
+        filterBtn.addEventListener("click", () => {
+            const allFilterBtn = document.querySelectorAll(".filter-btn");
+            filtre = filterBtn.textContent;
+            for (const currentFilterBtn of allFilterBtn) {
+              if (currentFilterBtn === filterBtn) {
+                currentFilterBtn.classList.add("active-filter-btn");
+              } else {
+                currentFilterBtn.classList.remove("active-filter-btn");
+              }
+            }
+            // ici la valeur filtre est correctement mise à jour quand je clique sur une catégorie mais pas à l'extérieur du addEventlisteber
+            //console.log(filtre)
+        });
+
+        //ici la valeur n'est pas mise à jour elle reste sur la valeur par défaut "Tout"
+        console.log(filtre)
+
+        const allFigure = document.querySelectorAll(".figure")
+       
+        for (const [index, figure] of allFigure.entries()) {
+            const category = data[index].category.name;
+            figure.setAttribute('data-category', category);
+            if (category === filtre || filtre === "Tout" ) {
+          figure.classList.remove("hidden")
+            } else {
+          figure.classList.add("hidden")
+            }
+        }
+
+
+      }
+      
+      filtrer()
       filterElement.appendChild(filterBtn);
     }
   } catch (err) {
